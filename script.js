535ad00a2e1813a99d9b0a8ec5c0c104bd53128f
theostavrides <theostavrides@gmail.com>
2019-07-03 20:32:54 -0700

    make demo beat

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //  --------------- SEQUENCER ----------------
 function createSequencer(parent, options){
-  const sequencerContainer = createSequencerContainer();
-  const buttonGrid         = createButtonGrid();
-  sequencerContainer.appendChild(buttonGrid);
+  const sequencer  = createSequencerDiv();
+  const buttonGrid = createButtonGrid();
+  sequencer.appendChild(buttonGrid);
   addButtons(buttonGrid);
   addSequencerClickEvents(buttonGrid);
-  parent.appendChild(sequencerContainer);
+  parent.appendChild(sequencer);
 }
 
-function createSequencerContainer(){
-  let sequencerContainer = document.createElement('div');
-  sequencerContainer.classList.add('sequencer', 'align-middle');
-  return sequencerContainer;
+function createSequencerDiv(){
+  let sequencer = document.createElement('div');
+  sequencer.classList.add('sequencer', 'align-middle');
+  return sequencer;
 }
 
 function createButtonGrid(){
@@ -44,10 +44,35 @@ function addSequencerClickEvents(buttonGrid){
   }
 }
 
+
 createSequencer(document.body);
 
+//---------------- DEMO SETUP ------------------
 
-//---------------- Audio ---------------
+let buttons = document.getElementsByClassName('buttonGrid')[0].childNodes;
+let sequencerData = [
+  [1,0,0,1,0,0],
+  [0,0,1,1,0,0],
+  [1,1,0,0,0,0],
+  [0,0,1,1,0,0],
+  [1,0,0,0,0,0],
+  [0,0,1,0,0,0],
+  [1,1,0,0,0,0],
+  [0,0,1,0,0,0],
+  [1,0,0,0,0,0],
+  [0,0,1,0,0,1],
+  [1,1,0,0,0,0],
+  [0,0,1,1,0,1],
+  [1,0,0,0,0,0],
+  [0,0,1,0,0,0],
+  [1,1,0,0,0,0],
+  [0,0,1,0,0,1],
+
+]
+
+
+
+//------------------ AUDIO ---------------------
 
 let kick = new Audio('808/kick.wav');
 let snare = new Audio('808/snare.mp3');
@@ -56,17 +81,32 @@ let hhato = new Audio('808/hhato.mp3');
 let clap = new Audio('808/clap.mp3');
 let clave = new Audio('808/clave.wav');
 
-setTimeout(()=>{
-  setInterval(()=>{kick.play()}, 1000);
-},1000)
 
-setTimeout(()=>{
-  setInterval(()=>{snare.play()}, 500);
-},1750)
+// -------------- SEQUENCER LOOP ---------------
 
-setTimeout(()=>{
-  setInterval(()=>{clap.play()}, 1000);
-},1750)
+let currentBeat = 0;
+
+function loop(){
+  playColumn(currentBeat);
+  console.log(currentBeat)
+  currentBeat >= 15 ? currentBeat = 0 : currentBeat++;
+}
+
+function playColumn(currentBeat){
+  const column = sequencerData[currentBeat];
+    if (column[0] === 1) kick.play();
+    if (column[1] === 1) snare.play();
+    if (column[2] === 1) hhatc.play();
+    if (column[3] === 1) hhato.play();
+    if (column[4] === 1) clap.play();
+    if (column[5] === 1) clave.play();
+}
+
+
+
+document.body.addEventListener('click', ()=>{
+  setInterval(loop, 250)
+})
 
 
 
