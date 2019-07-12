18bba7d6919883fb77e5bb22b0d3e17bc594d2cc
theostavrides <theostavrides@gmail.com>
2019-07-12 12:42:38 -0700

    change sample data structure

--- data.js
+++ data.js
@@ -28,26 +28,21 @@ class Instrument {
 class Piano extends Instrument {
   constructor(options = {}){
     super();
-    this.volume = 100;
-    this.reverb = {}
-    this.delay = {}
-    this.distortion = {}
-    this.chorus = {}
+    this._volume = 100;
+    this._reverb = {}
+    this._delay = {}
+    this._distortion = {}
+    this._chorus = {}
   }
 }
 
 class Sequencer extends Instrument {
   constructor(options = {}){
     super();
-    this.kit = '808';
-    this.sample0 = 0
-    this.sample1 = 1
-    this.sample2 = 2
-    this.sample3 = 3
-    this.sample4 = 4
-    this.sample5 = 5
-    this.volumes = [100,100,100,100,100,100];
-    this.pans    = [50, 50, 50, 50, 50, 50];
+    this._kit = '808';
+    this._samples = ['808clap.wav', '808kick.mp3', '808hhc.mp3', '808hho.mp3', '808clap.mp3', '808shaker.wav'];
+    this._volumes = [100, 100, 100, 100, 100, 100];
+    this._pans    = [50, 50, 50, 50, 50, 50];
   }
 }
 
