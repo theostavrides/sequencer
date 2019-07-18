7a7a16163abbfdd110615389cc4273540f021a91
theostavrides <theostavrides@gmail.com>
2019-07-17 17:20:46 -0700

    create basic sequencer options grid

--- js/options.js
+++ js/options.js
@@ -3,6 +3,8 @@ function createOptionsPanel(parent){
     const options    = createOptions();
     const tabs       = createTabs();
     const optionsBox = createOptionsBox();
+    const sequencerOptions = createSequencerOptions();
+    optionsBox.appendChild(sequencerOptions);
     options.appendChild(tabs);
     options.appendChild(optionsBox);
     parent.appendChild(options);
@@ -15,9 +17,9 @@ function createOptionsPanel(parent){
   }
 
   function createTabs(){
-    let tabs = document.createElement('div');
-    let tab1 = document.createElement('div');
-    let tab2 = document.createElement('div');
+    let tabs  = document.createElement('div');
+    let tab1  = document.createElement('div');
+    let tab2  = document.createElement('div');
     let text1 = document.createElement('p');
     let text2 = document.createElement('p');
     text1.innerHTML = 'sequencer';
@@ -38,6 +40,41 @@ function createOptionsPanel(parent){
     let optionsBox = document.createElement('div');
     optionsBox.id = 'optionsBox';
     return optionsBox;
+  }
+
+  function createSequencerOptions(){
+    let sequencerOptions = document.createElement('div');
+    let grid             = document.createElement('div');
+    sequencerOptions.id  = 'sequencerOptions';
+    grid.id              = 'sequencerOptionsGrid';
+    sequencerOptions.appendChild(grid);
+
+    //add table tible
+    let tableTitleText = ['row', 'sample', 'vol', 'pan']
+    for (let i = 0; i < 4; i++) {
+      const div = document.createElement('div');
+      div.classList.add('tableTitle');
+      div.innerHTML = tableTitleText[i];
+      grid.appendChild(div);
+    }
+    for (let i = 0; i < 7; i++) {
+      const rowNumber = document.createElement('div');
+      rowNumber.classList.add('rowNumber');
+      rowNumber.innerHTML = i + 1;
+      const sample = document.createElement('div');
+      sample.classList.add('sample');
+      const volume = document.createElement('div');
+      volume.classList.add('volume');
+      const pan = document.createElement('div');
+      pan.classList.add('pan');
+      grid.appendChild(rowNumber);
+      grid.appendChild(sample);
+      grid.appendChild(volume);
+      grid.appendChild(pan);
+    }
+
+
+    return sequencerOptions;
   }
 
   initialize();
