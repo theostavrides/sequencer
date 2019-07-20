04e0c033ba7ba503a178ba55a681ab4db4a7d05b
theostavrides <theostavrides@gmail.com>
2019-07-19 23:57:38 -0700

    add nav patterns click even handler

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,24 @@ function addPatternClickEvents(){
   let patternsBox = document.getElementById('patterns');
   patternsBox.addEventListener('click', handlePatternClick);
   function handlePatternClick(e){
-    console.log(e.target);
+    let patternBox = e.target.parentNode;
+    let index = Array.from(patternBox.parentNode.children).indexOf(patternBox) + 1;
+    if (index === 10) index = 0;
+
+    if (e.target.tagName === 'IMG') {
+      togglePatternOnOffState(index);
+    } else {
+      currentPatternView = index;
+    }
+
   }
+}
+
+
+function togglePatternOnOffState(patternNum){
+  patternOnOffState[patternNum] === 0 ?
+    patternOnOffState[patternNum] = 1 :
+    patternOnOffState[patternNum] = 0;
 }
 
 addPatternClickEvents();
