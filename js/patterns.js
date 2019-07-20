40392e2955bcf390cb4227a3c16b9d7bf03f892e
theostavrides <theostavrides@gmail.com>
2019-07-20 00:57:02 -0700

    make patterns switchable

---
+++ js/patterns.js
@@ -0,0 +1,44 @@
+function addPatternClickEvents(){
+  const patternsBox = document.getElementById('patterns');
+  patternsBox.addEventListener('click', handlePatternClick);
+  function handlePatternClick(e){
+    const patternBox = e.target.parentNode;
+    let index = Array.from(patternBox.parentNode.children).indexOf(patternBox) + 1;
+    if (index === 10) index = 0;
+
+    if (e.target.tagName === 'IMG') {
+      togglePatternOnOffState(index);
+    } else {
+      const oldPatternView = currentPatternView;
+      currentPatternView = index;
+      renderPattern(oldPatternView, currentPatternView);
+    }
+  }
+}
+
+function renderPattern(oldPatternNum, newPatternNum){
+  let newData = sequencerData[newPatternNum];
+  let sequencerButtons = document.getElementsByClassName('sequencerButton');
+  let prevPattern = sequencerData[oldPatternNum];
+  let newPattern  = sequencerData[newPatternNum];
+  console.log(prevPattern)
+  for (let column = 0; column < 16; column++) {
+    prevPattern[column].forEach(e => {
+      sequencerButtons.item((column) + (e * 16)).classList.remove('on')
+      sequencerButtons.item((column) + (e * 16)).classList.add('off')
+    })
+    newPattern[column].forEach(e => {
+      sequencerButtons.item((column) + (e * 16)).classList.remove('off')
+      sequencerButtons.item((column) + (e * 16)).classList.add('on')
+    })
+  }
+}
+
+
+function togglePatternOnOffState(patternNum){
+  patternOnOffState[patternNum] === 0 ?
+    patternOnOffState[patternNum] = 1 :
+    patternOnOffState[patternNum] = 0;
+}
+
+addPatternClickEvents();
