330083591bd3243a4d4a6f09766332ba6a8eb836
theostavrides <theostavrides@gmail.com>
2019-08-14 19:30:42 -0700

    clean up & refactor

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 //------------------ AUDIO ---------------------
 
+const samples = {
+  0: {kit: '808', name:'kick', sample: new Audio('samples/808/kick.wav'), volume: 50, pan: 50},
+  1: {kit: '808', name:'snare', sample: new Audio('samples/808/snare.mp3'), volume: 50, pan: 50},
+  2: {kit: '808', name:'hhatc', sample: new Audio('samples/808/hhatc.mp3'), volume: 50, pan: 50},
+  3: {kit: '808', name:'hhato', sample: new Audio('samples/808/hhato.mp3'), volume: 50, pan: 50},
+  4: {kit: '808', name:'clap', sample: new Audio('samples/808/clap.mp3'), volume: 50, pan: 50},
+  5: {kit: '808', name:'maraca', sample: new Audio('samples/808/maraca.wav'), volume: 50, pan: 50},
 
-let samples = {
-  0: {kit: '808', name:'kick', sample: new Audio('808/kick.wav'), volume: 50, pan: 50},
-  1: {kit: '808', name:'snare', sample: new Audio('808/snare.mp3'), volume: 50, pan: 50},
-  2: {kit: '808', name:'hhatc', sample: new Audio('808/hhatc.mp3'), volume: 50, pan: 50},
-  3: {kit: '808', name:'hhato', sample: new Audio('808/hhato.mp3'), volume: 50, pan: 50},
-  4: {kit: '808', name:'clap', sample: new Audio('808/clap.mp3'), volume: 50, pan: 50},
-  5: {kit: '808', name:'maraca', sample: new Audio('808/maraca.wav'), volume: 50, pan: 50},
+  6: {kit: 'blackwater', name: 'kick1', sample: new Audio('samples/blackwater/kick1.wav'), volume: 50, pan: 50},
+  7: {kit: 'blackwater', name: 'kick2', sample: new Audio('samples/blackwater/kick2.wav'), volume: 50, pan: 50},
+  8: {kit: 'blackwater', name: 'kick3', sample: new Audio('samples/blackwater/kick3.wav'), volume: 50, pan: 50},
+  9: {kit: 'blackwater', name: 'snare', sample: new Audio('samples/blackwater/snare.wav'), volume: 50, pan: 50},
+  10: {kit: 'blackwater', name: 'snare2', sample: new Audio('samples/blackwater/snare2.wav'), volume: 50, pan: 50},
+  11: {kit: 'blackwater', name: 'hhatc', sample: new Audio('samples/blackwater/hhc.wav'), volume: 50, pan: 50},
+  12: {kit: 'blackwater', name: 'hhato', sample: new Audio('samples/blackwater/hho.wav'), volume: 50, pan: 50},
+  13: {kit: 'blackwater', name: 'crash', sample: new Audio('samples/blackwater/crash.wav'), volume: 50, pan: 50},
+  14: {kit: 'blackwater', name: 'piano', sample: new Audio('samples/blackwater/piano.wav'), volume: 50, pan: 50},
 
-  6: {kit: 'blackwater', name: 'kick1', sample: new Audio('blackwater/kick1.wav'), volume: 50, pan: 50},
-  7: {kit: 'blackwater', name: 'kick2', sample: new Audio('blackwater/kick2.wav'), volume: 50, pan: 50},
-  8: {kit: 'blackwater', name: 'kick3', sample: new Audio('blackwater/kick3.wav'), volume: 50, pan: 50},
-  9: {kit: 'blackwater', name: 'snare', sample: new Audio('blackwater/snare.wav'), volume: 50, pan: 50},
-  10: {kit: 'blackwater', name: 'snare2', sample: new Audio('blackwater/snare2.wav'), volume: 50, pan: 50},
-  11: {kit: 'blackwater', name: 'hhatc', sample: new Audio('blackwater/hhc.wav'), volume: 50, pan: 50},
-  12: {kit: 'blackwater', name: 'hhato', sample: new Audio('blackwater/hho.wav'), volume: 50, pan: 50},
-  13: {kit: 'blackwater', name: 'crash', sample: new Audio('blackwater/crash.wav'), volume: 50, pan: 50},
-  14: {kit: 'blackwater', name: 'piano', sample: new Audio('blackwater/piano.wav'), volume: 50, pan: 50},
-
-  15: {kit: 'shadows', name: 'kick', sample: new Audio('shadows/kick.wav'), volume: 50, pan: 50},
-  16: {kit: 'shadows', name: 'hhatc', sample: new Audio('shadows/hhc.wav'), volume: 50, pan: 50},
-  17: {kit: 'shadows', name: 'snare', sample: new Audio('shadows/snare1.wav'), volume: 50, pan: 50},
-  18: {kit: 'shadows', name: 'rim', sample: new Audio('shadows/rim.wav'), volume: 50, pan: 50},
-  19: {kit: 'shadows', name: 'clap', sample: new Audio('shadows/clap.wav'), volume: 50, pan: 50},
-  20: {kit: 'shadows', name: 'tom', sample: new Audio('shadows/tom.wav'), volume: 50, pan: 50},
-  21: {kit: 'shadows', name: 'conga1', sample: new Audio('shadows/conga1.wav'), volume: 50, pan: 50},
-  22: {kit: 'shadows', name: 'conga2', sample: new Audio('shadows/conga2.wav'), volume: 50, pan: 50},
-  23: {kit: 'shadows', name: 'conga3', sample: new Audio('shadows/conga3.wav'), volume: 50, pan: 50},
+  15: {kit: 'shadows', name: 'kick', sample: new Audio('samples/shadows/kick.wav'), volume: 50, pan: 50},
+  16: {kit: 'shadows', name: 'hhatc', sample: new Audio('samples/shadows/hhc.wav'), volume: 50, pan: 50},
+  17: {kit: 'shadows', name: 'snare', sample: new Audio('samples/shadows/snare1.wav'), volume: 50, pan: 50},
+  18: {kit: 'shadows', name: 'rim', sample: new Audio('samples/shadows/rim.wav'), volume: 50, pan: 50},
+  19: {kit: 'shadows', name: 'clap', sample: new Audio('samples/shadows/clap.wav'), volume: 50, pan: 50},
+  20: {kit: 'shadows', name: 'tom', sample: new Audio('samples/shadows/tom.wav'), volume: 50, pan: 50},
+  21: {kit: 'shadows', name: 'conga1', sample: new Audio('samples/shadows/conga1.wav'), volume: 50, pan: 50},
+  22: {kit: 'shadows', name: 'conga2', sample: new Audio('samples/shadows/conga2.wav'), volume: 50, pan: 50},
+  23: {kit: 'shadows', name: 'conga3', sample: new Audio('samples/shadows/conga3.wav'), volume: 50, pan: 50},
 }
 
-var reverb = new Tone.JCReverb(0.9).connect(Tone.Master);
-var feedbackDelay = new Tone.FeedbackDelay("8n", 0.1).toMaster();
-var chorus = new Tone.Chorus(4, 2.5, 0.5);
-var vol = new Tone.Volume(-15).toMaster();
-let synth = new Tone.PolySynth(6, Tone.Synth).chain(vol, chorus, reverb, feedbackDelay);
-
+const reverb = new Tone.JCReverb(0.9).connect(Tone.Master);
+const feedbackDelay = new Tone.FeedbackDelay("8n", 0.1).toMaster();
+const chorus = new Tone.Chorus(4, 2.5, 0.5);
+const vol = new Tone.Volume(-15).toMaster();
+const synth = new Tone.PolySynth(6, Tone.Synth).chain(vol, chorus, reverb, feedbackDelay);
 
 
 //-----------------  STATE ----------------------
@@ -47,6 +45,19 @@ let patternOnOffState = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]; // which patterns are on
 let activeTabState = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //shows if sequencer or synth options tab is selected
 
 let selectedSampleDropDown = null;
+
+let pianoData = [
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+];
 
 let sequencerData = [
   [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
@@ -72,26 +83,15 @@ let sequencerSampleData = [
   [0,1,2,3,4,5],
   [0,1,2,3,4,5],
   [0,1,2,3,4,5],
-]
-
-let pianoData = [
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
-  [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
 ];
 
 
-let notes = ['B', 'A#', 'A', 'G#', 'G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C'];
+//-----------------  CONSTANTS ----------------------
+
+const notes = ['B', 'A#', 'A', 'G#', 'G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C'];
 
 
-// -------------- SEQUENCER LOOP ---------------
+// --------------- SEQUENCER LOOP -------------------
 
 function loop(){
   let currentBeat = 0;
