4c099c41fa38d076be8a7d4d2a195813fa7714b7
theostavrides <theostavrides@gmail.com>
2019-08-19 20:56:55 -0700

    change server listening message

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Jam.js server listening on port ${port}!`));
